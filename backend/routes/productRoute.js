df176ac7338caf853fe54619705a8bcd4aac0195
Nikhil Gupta <nikhilg7404@gmail.com>
2024-07-02 23:12:06 +0530

    Update productRoute

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -5,7 +5,6 @@ const router = express.Router();
 
 router.route("/products").get(getAllProducts)
 router.route("/products/new").post(createProducts)
-router.route("/products/new").post(createProducts)
 router.route("/products/:id").put(updateProduct).delete(deleteProduct)
 
 module.exports = router;
